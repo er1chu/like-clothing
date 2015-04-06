b845f27e1a6145fab44f4236335c0a6bcac60f2c
Eric Hu <ericjhu@gmail.com>
2015-04-06 13:10:52 -0400

    info box

--- js/app.js
+++ js/app.js
@@ -6,25 +6,22 @@ $(function () {
 			this._initCheckWidth();
 			this._initParallax();
 			this._initScrollFunctions();
+			this._initInformation();
 		},
 
 		_initScrollFunctions: function () {
-			
 			// Convert Vertical to Horizontal Scroll
 			scrollConverter.activate();
-
 			// Unblur Videos, Blur Logo on Scroll
 			$(window).scroll($.debounce( 250, true, function(){
 			    $('video').removeClass('big-wavy')
 			    $('.masthead').addClass('logo-wavy')
 			}));
-			
 			// Blur Videos, Unblur Logo on Scroll stop
 			$(window).scroll($.debounce( 250, function(){
 			     $('video').addClass('big-wavy')
 			     $('.masthead').removeClass('logo-wavy')
 			}));
-
 			// Loop scroll at end of document
 			$(window).scroll(function() {
 				 if ($(window).scrollLeft() + document.documentElement.clientWidth >= $(document).width()) {
@@ -44,9 +41,9 @@ $(function () {
 			$(window).load(function (){
 				calculateWidth()
 				// Content hidden until everything loaded
-				$('.wrapper').addClass('loaded');
-
+				$('.content').addClass('loaded');
 			});
+
 			$(window).resize($.debounce(500, calculateWidth));
 
 			function calculateWidth() {
@@ -61,6 +58,14 @@ $(function () {
 				$('.wrapper').css('width',i+'px');
 			}
 
+		},
+
+		_initInformation: function () {
+
+			$('.masthead').click(function () {
+				$('.info').addClass('info-loaded');
+				$('.content').addClass('content-blur')
+			})
 		}
 	};
 

a16e44714de972d72e4fe72de0b10c9412fad5e0
Eric Hu <ericjhu@gmail.com>
2015-04-06 11:03:12 -0400

    refactor JS

--- js/app.js
+++ js/app.js
@@ -4,60 +4,62 @@ $(function () {
 	var app = {
 		init: function () {
 			this._initCheckWidth();
-			this._initParallaxScroll();
-			this._initScrollConvert();
+			this._initParallax();
+			this._initScrollFunctions();
 		},
 
-		_initScrollConvert: function () {
-			scrollConverter.activate();
-		},
-
-		_initParallaxScroll: function () {
-			$('.masthead').parallax();
+		_initScrollFunctions: function () {
 			
-			// $(window).bind('scroll',function(e){
-			//     parallaxScroll();
-			// });
+			// Convert Vertical to Horizontal Scroll
+			scrollConverter.activate();
 
-			// $(window).scroll($.throttle( 500, scrollVideoThree));
-
+			// Unblur Videos, Blur Logo on Scroll
 			$(window).scroll($.debounce( 250, true, function(){
 			    $('video').removeClass('big-wavy')
 			    $('.masthead').addClass('logo-wavy')
 			}));
+			
+			// Blur Videos, Unblur Logo on Scroll stop
 			$(window).scroll($.debounce( 250, function(){
 			     $('video').addClass('big-wavy')
 			     $('.masthead').removeClass('logo-wavy')
 			}));
 
+			// Loop scroll at end of document
 			$(window).scroll(function() {
 				 if ($(window).scrollLeft() + document.documentElement.clientWidth >= $(document).width()) {
         			$(window).scrollLeft(0);
       			}
 			});
+		},
+
+		_initParallax: function () {
+			// Parallax Mouse movement
+			$('.masthead').parallax();
 
 		},
 
 		_initCheckWidth: function () {
 			
-			$(window).load(function() {
-				$('body').css('opacity','1');
-				var i = 0;
-				$('.piece').each(function(index,elem){
-					i = i + $(this).outerWidth(true) + 0.5;
-				});
-				$('.wrapper').css('width',i+'px');
-				// $lastImage = $('.wrapper img:last').position().left + $('.wrapper img:last').outerWidth(true) + 20;
-				// $('.wrapper').css('width', $lastImage + 'px')
-			});
+			$(window).load(function (){
+				calculateWidth()
+				// Content hidden until everything loaded
+				$('.wrapper').addClass('loaded');
 
-			$(window).resize($.debounce(500, function(){
+			});
+			$(window).resize($.debounce(500, calculateWidth));
+
+			function calculateWidth() {
+
+				// Initialize variable containing total width of all images
 				var i = 0;
+				// Loop through each image, calculate Width
 				$('.piece').each(function(index,elem){
 					i = i + $(this).outerWidth(true) + 0.5;
 				});
+				// Width of Wrapper is equal to Width of all images
 				$('.wrapper').css('width',i+'px');
-			}));
+			}
 
 		}
 	};
